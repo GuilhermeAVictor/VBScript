e3c5146d4deda5fe41438e7ba05f5d4355508a60
Guilherme Amaral <guilherme.victor@automapower.com.br>
2025-05-22 08:34:56 -0300

    m

--- GerarScript.vb
+++ GerarScript.vb
@@ -1,12 +1,12 @@
 ' Chamado da Função GerarScript
-' =GerarScript(B3; C3; D3; E3; F3; G3; H3; I3; J3; K3; L3; M3; N3; O3; P3; Q3; R3; S3; T3; U3; V3; W3; X3; Y3; Z3; AA3; AB3)
+' =GerarScript(B3; C3; D3; E3; F3; G3; H3; I3; J3; K3; L3; M3; N3; O3; P3; Q3; R3; S3; T3; U3; V3; W3; X3; Y3; Z3; AA3; AB3; AC3)
 
 Function GerarScript(TypeName As String, Propriedade1 As String, Propriedade2 As String, TextoAux As String, MetodoAux As String, _
     VerificarPropriedadeVazia As String, VerificarPropriedadeHabilitada As String, _
     VerificarPropriedadeCondicional As String, VerificarObjetoDesatualizado As String, VerificarPropriedadeValor As String, _
     VerificarPropriedadeTextoProibido As String, VerificarBancoDeDados As String, ContarObjetosDoTipo As String, _
     VerificarUserFields As String, VerificarCamposUsuariosServidorAlarmes As String, VerificarObjetoInternoIndevido As String, _
-    VerificarTipoPai As String, AreaDominio As String, AreaDrivers As String, AreaBancoDados As String, _
+    VerificarTipoPai As String, VerificarAssociacaoBase As String, AreaDominio As String, AreaDrivers As String, AreaBancoDados As String, _
     AreaBiblioteca As String, AreaFluxoDados As String, AreaEstruturaPastas As String, _
     AreaTelas As String, Aviso As String, erro As String, Revisar As String) As String
 
@@ -41,8 +41,10 @@ Function GerarScript(TypeName As String, Propriedade1 As String, Propriedade2 As
         funcao = "VerificarObjetoInternoIndevido"
     ElseIf VerificarTipoPai <> "" Then
         funcao = "VerificarTipoPai"
+    ElseIf VerificarAssociacaoBase <> "" Then
+        funcao = "VerificarAssociacaoBase"
     Else
-        GerarScript = "Objeto não possui verificações"
+        GerarScript = "'Objeto não possui verificações"
         Exit Function
     End If
 
@@ -86,20 +88,18 @@ Function GerarScript(TypeName As String, Propriedade1 As String, Propriedade2 As
             GerarScript = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, """ & Propriedade2 & """, " & TextoAux & ", """ & area & """, " & nivel
         Case "VerificarObjetoDesatualizado"
             GerarScript = funcao & " Obj, """ & TypeName & """, " & TextoAux & ", """ & area & """, " & nivel
-        Case "VerificarObjetoInternoIndevido"
+        Case "VerificarObjetoInternoIndevido", "ContarObjetosDoTipo", "VerificarUserFields", "VerificarCamposUsuariosServidorAlarmes"
             GerarScript = funcao & " Obj, """ & TypeName & """, """ & area & """, " & nivel
         Case "VerificarPropriedadeValor"
             GerarScript = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, " & TextoAux & ", " & CStr(MetodoAux) & ", """ & area & """, " & nivel
         Case "VerificarPropriedadeTextoProibido"
             GerarScript = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, " & TextoAux & ", """ & area & """, " & nivel
-        Case "VerificarBancoDeDados"
+        Case "VerificarBancoDeDados", "VerificarAssociacaoBase"
             GerarScript = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, """ & area & """, " & nivel
-        Case "ContarObjetosDoTipo", "VerificarUserFields", "VerificarCamposUsuariosServidorAlarmes"
-            GerarScript = funcao & " Obj, """ & TypeName & """, """ & area & """, " & nivel
         Case "VerificarTipoPai"
             GerarScript = funcao & " Obj, """ & TypeName & """, """ & TextoAux & """, " & CStr(MetodoAux) & ", """ & area & """, " & nivel
         Case Else
-            GerarScript = "Objeto não possui verificações"
+            GerarScript = "'Objeto não possui verificações"
     End Select
     Exit Function
 
