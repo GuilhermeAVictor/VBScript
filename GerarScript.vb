dc66df03e86874b5a1ef82eed90bd6556cb2cb78
Guilherme Amaral <guilherme.victor@automapower.com.br>
2025-05-28 11:56:36 -0300

    Update GerarScript.vb
    Adicionando a opção de definição de bloco e laço (DefinirBlocoLaco)

--- GerarScript.vb
+++ GerarScript.vb
@@ -1,22 +1,44 @@
-' Chamado da Função GerarScript
-' =GerarScript(B3; C3; D3; E3; F3; G3; H3; I3; J3; K3; L3; M3; N3; O3; P3; Q3; R3; S3; T3; U3; V3; W3; X3; Y3; Z3; AA3; AB3; AC3)
-
-Function GerarScript(TypeName As String, Propriedade1 As String, Propriedade2 As String, TextoAux As String, MetodoAux As String, _
+Function GerarScript(TypeName As String, Propriedade1 As String, Propriedade2 As String, TextoAux As String, MetodoAux As String, DefinirBlocoLaco As String, _
     VerificarPropriedadeVazia As String, VerificarPropriedadeHabilitada As String, _
     VerificarPropriedadeCondicional As String, VerificarObjetoDesatualizado As String, VerificarPropriedadeValor As String, _
     VerificarPropriedadeTextoProibido As String, VerificarBancoDeDados As String, ContarObjetosDoTipo As String, _
     VerificarUserFields As String, VerificarCamposUsuariosServidorAlarmes As String, VerificarObjetoInternoIndevido As String, _
     VerificarTipoPai As String, VerificarAssociacaoBase As String, AreaDominio As String, AreaDrivers As String, AreaBancoDados As String, _
     AreaBiblioteca As String, AreaFluxoDados As String, AreaEstruturaPastas As String, _
-    AreaTelas As String, Aviso As String, erro As String, Revisar As String) As String
+    AreaTelas As String, RelAviso As String, RelErro As String, RelRevisar As String, Observacao As String) As String
 
     On Error GoTo erro
 
     Dim funcao As String
     Dim area As String
     Dim nivel As String
+    Dim linhaFinal As String
 
-    ' Definir função com base em prioridade
+' === BLOCO ESPECIAL BASEADO NA COLUNA G ===
+Select Case DefinirBlocoLaco
+    Case "Iniciar área", "Finalizar área"
+        GerarScript = "  '=================================================================" & vbCrLf & _
+                      "  ' " & Observacao & vbCrLf & _
+                      "  '================================================================= "
+        Exit Function
+
+    Case "Else", "Finalizar laço(End If)"
+        Dim instrucao As String
+        If DefinirBlocoLaco = "Finalizar laço(End If)" Then
+            instrucao = "End If"
+        Else
+            instrucao = "Else"
+        End If
+
+        If Observacao <> "" Then
+            GerarScript = instrucao & " '" & Observacao
+        Else
+            GerarScript = instrucao
+        End If
+        Exit Function
+End Select
+
+    ' === DEFINIR FUNÇÃO ===
     If VerificarPropriedadeVazia <> "" Then
         funcao = "VerificarPropriedadeVazia"
     ElseIf VerificarPropriedadeHabilitada <> "" Then
@@ -44,11 +66,16 @@ Function GerarScript(TypeName As String, Propriedade1 As String, Propriedade2 As
     ElseIf VerificarAssociacaoBase <> "" Then
         funcao = "VerificarAssociacaoBase"
     Else
+    If Observacao <> "" Then
+        GerarScript = Observacao
+    Else
         GerarScript = "'Objeto não possui verificações"
-        Exit Function
+    End If
+    Exit Function
+
     End If
 
-    ' Definir área
+    ' === DEFINIR ÁREA ===
     If AreaDominio <> "" Then
         area = "Domínio"
     ElseIf AreaDrivers <> "" Then
@@ -67,40 +94,65 @@ Function GerarScript(TypeName As String, Propriedade1 As String, Propriedade2 As
         area = "Área não definida"
     End If
 
-    ' Definir nível
-    If Aviso <> "" Then
+    ' === DEFINIR NÍVEL ===
+    If RelAviso <> "" Then
         nivel = "0"
-    ElseIf erro <> "" Then
+    ElseIf RelErro <> "" Then
         nivel = "1"
-    ElseIf Revisar <> "" Then
+    ElseIf RelRevisar <> "" Then
         nivel = "2"
     Else
         nivel = "Nível não definido"
     End If
 
-    ' Montar script
+    ' === MONTAGEM DO SCRIPT ===
+    Dim flagFinal As String
+
+    If DefinirBlocoLaco = "Inicar laço(If ... Then)" Or DefinirBlocoLaco = "Condicioonar laço(ElseIf ... Then)" Then
+        flagFinal = "True"
+    ElseIf DefinirBlocoLaco = "" Then
+        flagFinal = "False"
+    Else
+        flagFinal = "" ' Para casos que não precisam de complemento
+    End If
+
     Select Case funcao
         Case "VerificarPropriedadeVazia"
-            GerarScript = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, """ & area & """, " & nivel
+            linhaFinal = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, """ & area & """, " & nivel
         Case "VerificarPropriedadeHabilitada"
-            GerarScript = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, " & TextoAux & ", """ & area & """, " & nivel
+            linhaFinal = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, " & TextoAux & ", """ & area & """, " & nivel
         Case "VerificarPropriedadeCondicional"
-            GerarScript = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, """ & Propriedade2 & """, " & TextoAux & ", """ & area & """, " & nivel
+            linhaFinal = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, """ & Propriedade2 & """, " & TextoAux & ", """ & area & """, " & nivel
         Case "VerificarObjetoDesatualizado"
-            GerarScript = funcao & " Obj, """ & TypeName & """, " & TextoAux & ", """ & area & """, " & nivel
+            linhaFinal = funcao & " Obj, """ & TypeName & """, " & TextoAux & ", """ & area & """, " & nivel
         Case "VerificarObjetoInternoIndevido", "ContarObjetosDoTipo", "VerificarUserFields", "VerificarCamposUsuariosServidorAlarmes"
-            GerarScript = funcao & " Obj, """ & TypeName & """, """ & area & """, " & nivel
+            linhaFinal = funcao & " Obj, """ & TypeName & """, """ & area & """, " & nivel
         Case "VerificarPropriedadeValor"
-            GerarScript = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, " & TextoAux & ", " & CStr(MetodoAux) & ", """ & area & """, " & nivel
+            linhaFinal = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, " & TextoAux & ", " & MetodoAux & ", """ & area & """, " & nivel
         Case "VerificarPropriedadeTextoProibido"
-            GerarScript = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, " & TextoAux & ", """ & area & """, " & nivel
+            linhaFinal = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, " & TextoAux & ", """ & area & """, " & nivel
         Case "VerificarBancoDeDados", "VerificarAssociacaoBase"
-            GerarScript = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, """ & area & """, " & nivel
+            linhaFinal = funcao & " Obj, """ & TypeName & """, """ & Propriedade1 & """, """ & area & """, " & nivel
         Case "VerificarTipoPai"
-            GerarScript = funcao & " Obj, """ & TypeName & """, """ & TextoAux & """, " & CStr(MetodoAux) & ", """ & area & """, " & nivel
+            linhaFinal = funcao & " Obj, """ & TypeName & """, """ & TextoAux & """, " & MetodoAux & ", """ & area & """, " & nivel
         Case Else
-            GerarScript = "'Objeto não possui verificações"
+            linhaFinal = "'Objeto não possui verificações"
     End Select
+
+    ' Finalizar
+    If flagFinal <> "" Then
+        linhaFinal = linhaFinal & ", " & flagFinal
+    End If
+
+If DefinirBlocoLaco = "Inicar laço(If ... Then)" Then
+    GerarScript = "If " & funcao & "(" & Mid(linhaFinal, Len(funcao) + 2) & ") Then"
+ElseIf DefinirBlocoLaco = "Condicioonar laço(ElseIf ... Then)" Then
+    GerarScript = "Else If " & funcao & "(" & Mid(linhaFinal, Len(funcao) + 2) & ") Then"
+
+    Else
+        GerarScript = linhaFinal
+    End If
+
     Exit Function
 
 erro:
